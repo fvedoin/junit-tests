150a82ad1beae5929b6fe2f681511d52b17cdb57
Fernando Vedoin Garcia <fernando.vgarcia@hotmail.com>
2021-01-08 00:45:34 -0300

    Running Data Driven tests using data from CSV file.

--- src/test/java/exercise10/AgeVotingValidation_CSV.java
+++ src/test/java/exercise10/AgeVotingValidation_CSV.java
@@ -1,4 +1,21 @@
 package exercise10;
 
+import junitparams.FileParameters;
+import junitparams.JUnitParamsRunner;
+import junitparams.mappers.CsvWithHeaderMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import voting.Voting;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(JUnitParamsRunner.class)
 public class AgeVotingValidation_CSV {
+
+    @Test
+    @FileParameters(value = "src/test/resources/data.csv",
+            mapper = CsvWithHeaderMapper.class)
+    public void validateMandatoryVoting(String name, int birthYear, String result) {
+        assertEquals(result, Voting.canVote(name, birthYear));
+    }
 }
--- src/test/java/exercise9/AgeVotingValidation_Parameters.java
+++ src/test/java/exercise9/AgeVotingValidation_Parameters.java
@@ -1,4 +1,39 @@
 package exercise9;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.*;
+import voting.Voting;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class)
 public class AgeVotingValidation_Parameters {
+
+    @Parameter(0) public String name;
+    @Parameter(1) public int birthYear;
+    @Parameter(2) public String result;
+
+    @Test
+    public void validateMandatoryVoting() {
+        assertEquals(result, Voting.canVote(name, birthYear));
+    }
+
+    @Parameters(name = "{0} | {1} | {2}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"Fernando", 2007, "Fernando, you cant vote"},
+                {"Fernando", 2006, "Fernando, you cant vote"},
+                {"Fernando", 2005, "Fernando, your vote is optional"},
+                {"Fernando", 2004, "Fernando, your vote is optional"},
+                {"Fernando", 2003, "Fernando, your vote is mandatory"},
+                {"Fernando", 1996, "Fernando, your vote is mandatory"},
+                {"Fernando", 1951, "Fernando, your vote is mandatory"},
+                {"Fernando", 1950, "Fernando, your vote is optional"}
+        });
+    }
 }
